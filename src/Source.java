3d9521aaa23c1fc2429f8ec96cd6273248cd48a6
mzitow <hhbaraka0@gmail.com>
2021-02-21 00:01:48 +0200

    add edit comment

--- src/Source.java
+++ src/Source.java
@@ -3,6 +3,7 @@ public class Source {
     public static void main(String[] args)
     {
         //this is a comment
+        //welcome keep adding comments
         System.out.println("Welcome to Java!");
     }
 }
