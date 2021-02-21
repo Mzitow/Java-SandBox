894c73f965fe0b1b25f4c89e30e7bff0d32cb12b
ismaaaa <ismahassan98@gmail.com>
2021-02-21 00:30:06 -0800

    Ismails Edit

--- src/Source.java
+++ src/Source.java
@@ -2,6 +2,7 @@ public class Source {
 
     public static void main(String[] args)
     {
+        System.out.println("The champ is front");
         System.out.println("Welcome to Java!");
     }
 }
