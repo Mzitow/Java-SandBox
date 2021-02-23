244f89e6f62250de0d40568416253665d44ebf6e
mzitow <hhbaraka0@gmail.com>
2021-02-23 17:26:32 +0200

    Quadratic Assignment

--- src/Source.java
+++ src/Source.java
@@ -2,8 +2,30 @@ public class Source {
 
     public static void main(String[] args)
     {
-        //this is a comment
-        //welcome keep adding comments
-        System.out.println("Welcome to Java!");
+        //@TODO PLEASE CREATE A NEW BRANCH BEFORE SOLVING THIS ASSIGNMENT
+        //@TODO Call the quadratic function here like SO :  solveQuadratic(1, -6, 9);
+        //@TODO Push your results
+
+    }
+
+    public static void solveQuadratic(int a, int b, int c)
+    {
+        /*
+        * Lets see the logic of the Quadratic Equation
+        *
+        *  (-b + root( (b ^ 2) - (4 * a * c)) ) / 2 * a
+        *               and
+        *  (-b - root( (b ^ 2) - (4 * a * c)) ) / 2 * a
+        *
+        *  This gives the two answers
+        *  @TODO  1. Check if the  (b^2 - 4 * a * c) is negative. (it will not be solvable, no real roots) : Display a message
+        *         2. If the result of (b^2 - 4 * a * c) is positive, then go ahead to solve it
+        *         3. Do this by saving the root( b ^ 2  - 4 * a * c) into a  new variable : double
+        *         4. Your equation become   (-b + your_new_variable ) / 2 * a
+        *         5. Create two other variables: double, to save answer_1 and answer_2
+        *         6. Display your results
+        *
+        * Your answers should be in decimals
+        * */
     }
 }
